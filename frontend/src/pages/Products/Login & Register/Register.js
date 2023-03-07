c170a03ba7850c0c8b63d29a7376738d7fbb1421
Nour Asma <nasma9521@gmail.com>
2023-03-07 17:12:23 +0200

    finish the register page

--- frontend/src/pages/Products/Login & Register/Register.js
+++ frontend/src/pages/Products/Login & Register/Register.js
@@ -1,7 +1,27 @@
 import React from "react";
+import styles from "./Authentication.module.css";
+import InputBlock from "./InputBlock";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <main className={` container ${styles.authentication}`}>
+      <h1 className={styles.register}>Register</h1>
+
+      <form>
+        <InputBlock type="text" name="name" lable="Name" />
+        <InputBlock type="text" name="email" lable="Email" />
+        <InputBlock type="password" name="password" lable="Password" />
+        <InputBlock
+          type="password"
+          name="password_confirmation"
+          lable="Confirm Passowrd"
+        />
+        <button type="submit" className="sub-button">
+          Craete Account
+        </button>
+      </form>
+    </main>
+  );
 };
 
 export default Register;
